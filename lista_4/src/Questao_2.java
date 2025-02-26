40213900a4d9e091db954a012e8c11b72b57087e
Carlos Henrique S S S <carloshsantiago360@gmail.com>
2025-02-26 16:06:50 -0300

    questao 2

--- lista_4/src/Questao_2.java
+++ lista_4/src/Questao_2.java
@@ -5,9 +5,26 @@ colocar a letra da música em ordem inversa, ou seja, a última linha da letra d
 deve ser a primeira a ser exibida e vice-versa*/
 
 
+import java.util.Scanner;
 
 public class Questao_2 {
     public static void executar_Q2() {
+        Scanner scan= new Scanner(System.in);
+        String[] linhaMusica = new String[10];
+        int contador = 0;
+        System.out.println("Vamos brincar com música");
 
+        while (contador < 10){
+            System.out.printf("Digite a %d° linha da música %n", (contador + 1));
+            linhaMusica[contador] = scan.nextLine();
+            contador++;
+        }
+        contador = 9;
+
+        while (contador >=0 ) {
+            System.out.println(linhaMusica[contador]);
+            contador--;
+
+        }
     }
 }
