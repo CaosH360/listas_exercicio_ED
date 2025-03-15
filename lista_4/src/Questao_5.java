63bd1c38b1eec190a672ef152878449cabc19371
Carlos Henrique S S S <carloshsantiago360@gmail.com>
2025-03-14 21:35:37 -0300

    questao 5

--- lista_4/src/Questao_5.java
+++ lista_4/src/Questao_5.java
@@ -4,10 +4,18 @@ Uma das maiores varejistas do mercado resolveu fazer uma promoção onde os clie
 com descontos incríveis disponíveis. Você deve apenas ler o preço dos produtos na sequência e apresentar o total da
 compra do cliente.*/
 
-
+import java.util.Scanner;
 
 public class Questao_5 {
     public static void executar_Q5() {
-
+        double[] precosProdutos = new double[5];
+        Scanner scan = new Scanner(System.in);
+        double totalCompra = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite o valor do produto " + (i + 1));
+            precosProdutos[i] = scan.nextDouble();
+            totalCompra = totalCompra + precosProdutos[i];
+        }
+        System.out.printf("O total da compra foi de R$%.2f", totalCompra);
     }
 }
